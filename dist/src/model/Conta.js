d3c8e698961bceab1896d25843bbaeb8a82f30cd
Juan Pablo <prof.juanpablo@gmail.com>
2025-02-04 22:15:24 -0300

    primeiro commit

---
+++ dist/src/model/Conta.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Conta = void 0;
+class Conta {
+    sacar() {
+        return 2000;
+    }
+}
+exports.Conta = Conta;
